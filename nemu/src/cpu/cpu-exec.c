08685cfec6bb168dcdc2ee707aecba4a4cf638ae
pcw <pcw760404931@163.com>
2025-01-06 16:41:48 +0800

    PA2-2:ftrace

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -151,7 +151,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-    //if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0) {
+      //if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0) {
           #ifdef CONFIG_ITRACE
               print_iringbuf();
           #endif
@@ -164,9 +164,8 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-          if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0)
+      if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0)
           print_iringbuf();
-          
       // fall through
     case NEMU_QUIT: statistic();
   }
