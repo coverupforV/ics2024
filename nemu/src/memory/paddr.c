9cffa233936901b27fbdb90823c8b7b8250cc511
tracer-ics2024 <tracer@njuics.org>
2025-01-06 14:41:48 +0800

    >  compile NEMU 202111998 pcw Linux pcw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:48 up  2:16,  1 user,  load average: 0.36, 0.18, 0.07

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -71,6 +71,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+      Log("Write memory at %#.8x for %d bytes: %x.", addr, len, data);
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
