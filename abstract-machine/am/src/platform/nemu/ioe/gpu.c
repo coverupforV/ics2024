c352ef10791c392dc54aea6e15844cd2ab34de1b
pcw <pcw760404931@163.com>
2025-01-18 11:01:55 +0800

    pa2-3:vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,9 +1,18 @@
+#include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t h = screen_wh & 0xffff;
+  uint32_t w = screen_wh >> 16;
+  int i;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -12,6 +21,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .width = 0, .height = 0,
     .vmemsz = 0
   };
+  uint32_t w_h = inl(VGACTL_ADDR);
+  cfg->width = (int)(w_h >> 16);
+  cfg->height = (int) (w_h & 0xffff);
+  cfg->vmemsz = 0; //cfg->height * cfg->width * sizeof(uint32_t);
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
